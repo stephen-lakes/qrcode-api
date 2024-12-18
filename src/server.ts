7ca669a270e7fc0ce035149edb9b85fcd3ed20d3
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-18 19:34:00 +0100

    include link to the api docs

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import app from './app';
+import app from "./app";
 
 const port = process.env.PORT || 3000;
 
 // Start the server
 app.listen(port, () => {
   console.log(`QR Code API running at http://localhost:${port}`);
+  console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
 });

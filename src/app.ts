3552209ae0dcbd4e4f098c0c9f0cd8b73665e7dc
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-18 19:12:40 +0100

    generate png

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import path from "path";
 import QRCode from "qrcode";
 
 const app: Application = express();
@@ -25,4 +26,36 @@ app.post("/generate", async (req, res) => {
     res.status(500).json({ error: "Unable to generate QRCode" });
   }
 });
+
+app.post("/generate-file", async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    if (!text) {
+      res.status(400).send({ error: "Text is required" });
+      return;
+    }
+    const qrCodeImageUrl = await QRCode.toDataURL(text);
+
+    const htmlResponse = ` 
+    <img src="${qrCodeImageUrl}" alt="QR Code"> 
+    // <!DOCTYPE html> 
+    //     <html lang="en"> 
+    //         <head> 
+    //             <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    //             <title>QR Code</title> 
+    //         </head> 
+    //         <body> 
+    //             <h1>Your QR Code</h1> 
+    //             <img src="${qrCodeImageUrl}" alt="QR Code"> 
+    //         </body> 
+    //     </html> 
+    `;
+
+    res.send({ message: "QR Code generated successfully", htmlResponse });
+  } catch (error) {
+    res.status(500).json({ error: "Unable to generate QRCode" });
+  }
+});
+
 export default app;
